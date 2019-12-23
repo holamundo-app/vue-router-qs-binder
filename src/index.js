b334185ef38f545ed0912d32c7a497201e091029
Brad Adams <hi@breadadams.com>
2019-12-23 16:48:36 +0100

    General improvements
    + Improve data comparisons, avoid unwanted empty query parameters
    + Misc. refactoring

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // @flow
-import { debounce, isFunction, isEqual, isEmpty } from '@/utils';
+import { debounce, isFunction, isEqual, isEmpty, pickBy } from '@/utils';
 
 type QsBinder = ({
   data: Object => Object | Object,
@@ -9,6 +9,17 @@ type QsBinder = ({
   updateDataMethodName?: string
 }) => Object;
 
+const getUpdatedValues = (
+  newData: Object,
+  previousData: Object,
+  queryData: Object
+): Object =>
+  pickBy(
+    newData,
+    (value, key) =>
+      !isEqual(value, previousData[key]) && !isEqual(value, queryData[key])
+  );
+
 const qsBinder: QsBinder = ({
   data,
   dataKey,
@@ -16,10 +27,12 @@ const qsBinder: QsBinder = ({
   didUpdate,
   updateDataMethodName = 'updateQsData'
 }) => {
+  const formatData = (input, param) => (isFunction(data) ? data(param) : input);
+
   return {
     data() {
       return {
-        [dataKey]: !isFunction(data) ? data : data(this.$route),
+        [dataKey]: formatData(data, this.$route),
         qsBinderHasMounted: false,
         qsBinderIsSilent: false
       };
@@ -59,12 +72,14 @@ const qsBinder: QsBinder = ({
           const { query, ...$route } = this.$route;
 
           if (this.qsBinderHasMounted && !isEqual(newValue, prevValue)) {
-            if (!isEqual(newValue, query)) {
+            const updatedValues = getUpdatedValues(newValue, prevValue, query);
+
+            if (!isEmpty(updatedValues)) {
               this.$router.push({
                 ...$route,
                 query: {
                   ...query,
-                  ...newValue
+                  ...updatedValues
                 }
               });
             }
@@ -82,13 +97,28 @@ const qsBinder: QsBinder = ({
             !isEqual(newRoute.query, this[dataKey])
           ) {
             if (!isEmpty(newRoute.query)) {
-              const newData = !isFunction(data)
-                ? newRoute.query
-                : data(newRoute);
+              const newData = formatData(newRoute.query, newRoute);
 
+              const resetItems = Object.keys(oldRoute.query).reduce(
+                (acc, key) => {
+                  if (!newData[key] && this[dataKey].hasOwnProperty(key)) {
+                    const defaultValue = formatData(data, newRoute)[key];
+
+                    return {
+                      ...acc,
+                      [key]: defaultValue
+                    };
+                  }
+
+                  return acc;
+                },
+                {}
+              );
+
+              this[updateDataMethodName](resetItems, true);
               this[updateDataMethodName](newData);
             } else {
-              this[dataKey] = {};
+              this[updateDataMethodName](formatData(data, newRoute), true);
             }
           }
         },
--- src/utils.js
+++ src/utils.js
@@ -2,3 +2,4 @@ export { default as debounce } from 'lodash/debounce';
 export { default as isFunction } from 'lodash/isFunction';
 export { default as isEqual } from 'lodash/isEqual';
 export { default as isEmpty } from 'lodash/isEmpty';
+export { default as pickBy } from 'lodash/pickBy';
